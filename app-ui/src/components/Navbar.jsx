1379fbb04698302b64fe761a05d40753baffbb60
Joyal15 <joyalbijujose@gmail.com>
2024-08-25 21:29:21 +0530

    Commit 1:Just built the basic React app

---
+++ app-ui/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="container">
+            <div className="logo"></div>
+            <div className="links"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
