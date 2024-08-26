13b361ca3d83a156e02b85484e9cd42a5f8ca963
Joyal <joyalbijujose@gmail.com>
2024-08-26 12:23:01 +0530

    Commit 2:A bit for now but ok

--- app-ui/src/components/Navbar.jsx
+++ app-ui/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Navbar.css' 
 
 function Navbar() {
   return (
     <div className='navbar'>
         <div className="container">
-            <div className="logo"></div>
+            <div className="logo">navbar</div>
             <div className="links"></div>
         </div>
     </div>
